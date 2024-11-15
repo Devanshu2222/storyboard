e7237b0efedd0c0def6878b6c3034e0515e48c54
Devanshu Singh <devanshu9992@gmail.com>
2024-11-15 13:04:43 +0530

    Initial Commit

---
+++ AppGlintTests/AppGlintTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppGlintTests.swift
+//  AppGlintTests
+//
+//  Created by Devanshu Singh(chitkara)     on 15/11/24.
+//
+
+import Testing
+@testable import AppGlint
+
+struct AppGlintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
